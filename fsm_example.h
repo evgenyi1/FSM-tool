9cbbf44d0717e9c4dd3f1b1c513a3ed114db7b12
leonidc <leonidc@mellanox.com>
2013-12-31 12:44:37 +0200

    uncrustify

--- fsm_example.h
+++ fsm_example.h
@@ -13,54 +13,55 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
-//***************************************************************
-// File: LagFsm.h
-// Class/Module: LagMngr
-//
-// Purpose:
-// Description:  Lag  state machine implementation
-//
-// Modification history:
-//
-//***************************************************************
+
+/* *************************************************************** */
+/* File: LagFsm.h */
+/* Class/Module: LagMngr */
+/*  */
+/* Purpose: */
+/* Description:  Lag  state machine implementation */
+/*  */
+/* Modification history: */
+/*  */
+/* *************************************************************** */
 #ifndef _LAG_FSM_H
 #define _LAG_FSM_H
 
 #include "fsm_tool_framework.h"
 
-// only example - ConcreteTimer  is real object that aggregates TimerMngr . InitFunc  fills pointers to functions by values of real functions .
-// Framework will work with these functions
-//#define ConcreteTimer TimerMngr
+/* only example - ConcreteTimer  is real object that aggregates TimerMngr . InitFunc  fills pointers to functions by values of real functions . */
+/* Framework will work with these functions */
+/* #define ConcreteTimer TimerMngr */
 
 /*#$*//*----------------------------------------------------------------
-            Generated enumerator for  States of FSM   
----------------------------------------------------------------*/
- enum lag_fsm_states_t{
-    lag_fsm_idle  = 0, 
-    lag_fsm_admin_down  = 1, 
-    lag_fsm_admin_up  = 2, 
-    lag_fsm_lag_down  = 3, 
-    lag_fsm_wait_lag_up  = 4, 
-    lag_fsm_lag_up  = 5, 
-    lag_fsm_condit1  = 6, 
-    lag_fsm_mismatch  = 7, 
- };
+            Generated enumerator for  States of FSM
+   ---------------------------------------------------------------*/
+enum lag_fsm_states_t {
+    lag_fsm_idle = 0,
+    lag_fsm_admin_down = 1,
+    lag_fsm_admin_up = 2,
+    lag_fsm_lag_down = 3,
+    lag_fsm_wait_lag_up = 4,
+    lag_fsm_lag_up = 5,
+    lag_fsm_condit1 = 6,
+    lag_fsm_mismatch = 7,
+};
 struct lag_fsm;
 /*----------------------------------------------------------------
-            Events of FSM   
----------------------------------------------------------------*/
- fsm_consume_results  lag_fsm_start_ev(struct lag_fsm  * fsm);
- fsm_consume_results  lag_fsm_port_up_ev(struct lag_fsm  * fsm,  int  lacp_enable,  int*  port_id);
- fsm_consume_results  lag_fsm_port_down_ev(struct lag_fsm  * fsm);
- fsm_consume_results  lag_fsm_lacp_up_ev(struct lag_fsm  * fsm,  int*  port_id);
- fsm_consume_results  lag_fsm_lacp_down_ev(struct lag_fsm  * fsm,  int*  port_id);
- fsm_consume_results  lag_fsm_mismatch_ev(struct lag_fsm  * fsm,  int*  port_id);
- fsm_consume_results  lag_fsm_admin_on_ev(struct lag_fsm  * fsm);
- fsm_consume_results  lag_fsm_admin_off_ev(struct lag_fsm  * fsm);
+            Events of FSM
+   ---------------------------------------------------------------*/
+fsm_consume_results lag_fsm_start_ev(struct lag_fsm  * fsm);
+fsm_consume_results lag_fsm_port_up_ev(struct lag_fsm  * fsm,  int lacp_enable,
+                                       int*  port_id);
+fsm_consume_results lag_fsm_port_down_ev(struct lag_fsm  * fsm);
+fsm_consume_results lag_fsm_lacp_up_ev(struct lag_fsm  * fsm,  int*  port_id);
+fsm_consume_results lag_fsm_lacp_down_ev(struct lag_fsm  * fsm, int*  port_id);
+fsm_consume_results lag_fsm_mismatch_ev(struct lag_fsm  * fsm,  int*  port_id);
+fsm_consume_results lag_fsm_admin_on_ev(struct lag_fsm  * fsm);
+fsm_consume_results lag_fsm_admin_off_ev(struct lag_fsm  * fsm);
 /*----------------------------------------------------------------
-              Getters for each State   
----------------------------------------------------------------*/
+              Getters for each State
+   ---------------------------------------------------------------*/
 tbool lag_fsm_idle_in(struct lag_fsm *fsm);
 tbool lag_fsm_admin_down_in(struct lag_fsm *fsm);
 tbool lag_fsm_admin_up_in(struct lag_fsm *fsm);
@@ -70,23 +71,23 @@ tbool lag_fsm_lag_up_in(struct lag_fsm *fsm);
 tbool lag_fsm_condit1_in(struct lag_fsm *fsm);
 tbool lag_fsm_mismatch_in(struct lag_fsm *fsm);
 /*----------------------------------------------------------------
-              Constructor of FSM   
----------------------------------------------------------------*/
-int lag_fsm_init(struct lag_fsm *fsm, fsm_user_trace user_trace, void * sched_func, void * unsched_func);
+              Constructor of FSM
+   ---------------------------------------------------------------*/
+int lag_fsm_init(struct lag_fsm *fsm, fsm_user_trace user_trace,
+                 void * sched_func, void * unsched_func);
 /*###############################################################
-            ###### declaration of FSM  #########  
-//################################################################*/
-typedef struct lag_fsm  
- {
-  fsm_base base;
-  int state_timer[8];
+ ###### declaration of FSM  #########
+   //################################################################*/
+typedef struct lag_fsm {
+    fsm_base base;
+    int state_timer[8];
 /*----------------------------------------------------------------
-           Private attributes :  
----------------------------------------------------------------*/
- /*#$*/
-  void * lag ;
- }lag_fsm;
+           Private attributes :
+   ---------------------------------------------------------------*/
+/*#$*/
+    void * lag;
+}lag_fsm;
 
- void     UserTrace        ( void* theBuf, int theLen );
+void UserTrace( void* theBuf, int theLen );
 
-#endif // _LAG_FSM_H
+#endif /* _LAG_FSM_H */
--- fsm_tool_simulator.c
+++ fsm_tool_simulator.c
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -25,53 +25,57 @@
 #include "fsm_example.h"
 
 void FsmTrace(char* buf, int len);
-int  MlnxTmUnsched(void ** timer_handler);
-int  MLnxTmSched(int theTimeout, void *data, void ** timer_handler);
+int MlnxTmUnsched(void ** timer_handler);
+int MLnxTmSched(int theTimeout, void *data, void ** timer_handler);
 
-void FsmTrace(char* buf, int len)
+void
+FsmTrace(char* buf, int len)
 {
-   printf("%s",buf);
+    printf("%s", buf);
 }
 
-int MlnxTmUnsched(void ** timer_handler)
+int
+MlnxTmUnsched(void ** timer_handler)
 {
-	return 1;
+    return 1;
 }
 
-int MLnxTmSched(int theTimeout, void *data, void ** timer_handler)
+int
+MLnxTmSched(int theTimeout, void *data, void ** timer_handler)
 {
-	return 1;
+    return 1;
 }
 
-int main(void)
+int
+main(void)
 {
-	int err = 0;
-	lag_fsm *pFsm = (lag_fsm *)malloc(sizeof(lag_fsm));
+    int err = 0;
+    lag_fsm *pFsm = (lag_fsm *)malloc(sizeof(lag_fsm));
 
-	printf("********** Start FSM Simulator ************\n");
+    printf("********** Start FSM Simulator ************\n");
 
-	if (!pFsm) {
-		return err;
-	}
+    if (!pFsm) {
+        return err;
+    }
 
-	pFsm->base.fsm_type = 2;
-    err = lag_fsm_init (pFsm, FsmTrace, MLnxTmSched, MlnxTmUnsched);
+    pFsm->base.fsm_type = 2;
+    err = lag_fsm_init(pFsm, FsmTrace, MLnxTmSched, MlnxTmUnsched);
 
-    lag_fsm_start_ev    (pFsm);
-	lag_fsm_start_ev    (pFsm);
-	lag_fsm_admin_on_ev  (pFsm);
-	lag_fsm_admin_off_ev (pFsm);
-	lag_fsm_admin_on_ev  (pFsm);
-	lag_fsm_lacp_up_ev   (pFsm,0);
-	
-	lag_fsm_port_down_ev (pFsm);
-	lag_fsm_port_up_ev   (pFsm,1,0);
-	lag_fsm_lacp_up_ev   (pFsm,0);
-	lag_fsm_mismatch_ev (pFsm,0);
+    lag_fsm_start_ev(pFsm);
+    lag_fsm_start_ev(pFsm);
+    lag_fsm_admin_on_ev(pFsm);
+    lag_fsm_admin_off_ev(pFsm);
+    lag_fsm_admin_on_ev(pFsm);
+    lag_fsm_lacp_up_ev(pFsm, 0);
 
-	lag_fsm_lacp_down_ev (pFsm,0);
-	lag_fsm_port_up_ev   (pFsm,1,0);
-	fsm_timer_trigger_operation(&pFsm->base, 4);
-	getchar();
-	return err;
+    lag_fsm_port_down_ev(pFsm);
+    lag_fsm_port_up_ev(pFsm, 1, 0);
+    lag_fsm_lacp_up_ev(pFsm, 0);
+    lag_fsm_mismatch_ev(pFsm, 0);
+
+    lag_fsm_lacp_down_ev(pFsm, 0);
+    lag_fsm_port_up_ev(pFsm, 1, 0);
+    fsm_timer_trigger_operation(&pFsm->base, 4);
+    getchar();
+    return err;
 }
